9377b85bc3e7ac43d643a6d08451a64c2975a93c
Lucas Zanek <lucaszanek94@gmail.com>
2022-10-12 11:46:27 -0300

    [CONFIG]: Pre commit hooks working

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,8 @@
-import { GraphQLClient } from "graphql-request";
-import { QueryClient } from "react-query";
-import { getSdk } from "./generated/graphql";
+import { GraphQLClient } from 'graphql-request';
+import { QueryClient } from 'react-query';
+import { getSdk } from './generated/graphql';
 
-const graphqlSchema = "http://localhost:4000/graphql";
+const graphqlSchema = 'http://localhost:4000/graphql';
 
 const gqlClient = new GraphQLClient(graphqlSchema);
 export const { findAll, getInvestment } = getSdk(gqlClient);
--- src/generated/graphql.ts
+++ src/generated/graphql.ts
@@ -3,9 +3,15 @@ import * as Dom from 'graphql-request/dist/types.dom';
 import gql from 'graphql-tag';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Exact<T extends { [key: string]: unknown }> = {
+  [K in keyof T]: T[K];
+};
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]?: Maybe<T[SubKey]>;
+};
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & {
+  [SubKey in K]: Maybe<T[SubKey]>;
+};
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -54,31 +60,25 @@ export type Mutation = {
   signinpLocal: LoginResponse;
 };
 
-
 export type MutationCreateInvestmentArgs = {
   input: CreateInvestmentInput;
 };
-
 
 export type MutationLogoutArgs = {
   loginUserInput: LoginUserInput;
 };
 
-
 export type MutationRefreshTokensArgs = {
   loginUserInput: LoginUserInput;
 };
-
 
 export type MutationRegisterUserArgs = {
   input: CreateUserInput;
 };
 
-
 export type MutationSignUpLocalArgs = {
   loginUserInput: LoginUserInput;
 };
-
 
 export type MutationSigninpLocalArgs = {
   loginUserInput: LoginUserInput;
@@ -106,47 +106,87 @@ export type UserSecured = {
   username?: Maybe<Scalars['String']>;
 };
 
-export type FindAllQueryVariables = Exact<{ [key: string]: never; }>;
+export type FindAllQueryVariables = Exact<{ [key: string]: never }>;
 
+export type FindAllQuery = {
+  __typename?: 'Query';
+  findAll: Array<{ __typename?: 'User'; username: string }>;
+};
 
-export type FindAllQuery = { __typename?: 'Query', findAll: Array<{ __typename?: 'User', username: string }> };
+export type GetInvestmentQueryVariables = Exact<{ [key: string]: never }>;
 
-export type GetInvestmentQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-export type GetInvestmentQuery = { __typename?: 'Query', getInvestment: Array<{ __typename?: 'Investment', title: string, description: string, _id: string }> };
-
+export type GetInvestmentQuery = {
+  __typename?: 'Query';
+  getInvestment: Array<{
+    __typename?: 'Investment';
+    title: string;
+    description: string;
+    _id: string;
+  }>;
+};
 
 export const FindAllDocument = gql`
-    query findAll {
-  findAll {
-    username
-  }
-}
-    `;
-export const GetInvestmentDocument = gql`
-    query getInvestment {
-  getInvestment {
-    title
-    description
-    _id
-  }
-}
-    `;
-
-export type SdkFunctionWrapper = <T>(action: (requestHeaders?:Record<string, string>) => Promise<T>, operationName: string, operationType?: string) => Promise<T>;
-
-
-const defaultWrapper: SdkFunctionWrapper = (action, _operationName, _operationType) => action();
-
-export function getSdk(client: GraphQLClient, withWrapper: SdkFunctionWrapper = defaultWrapper) {
-  return {
-    findAll(variables?: FindAllQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<FindAllQuery> {
-      return withWrapper((wrappedRequestHeaders) => client.request<FindAllQuery>(FindAllDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'findAll', 'query');
-    },
-    getInvestment(variables?: GetInvestmentQueryVariables, requestHeaders?: Dom.RequestInit["headers"]): Promise<GetInvestmentQuery> {
-      return withWrapper((wrappedRequestHeaders) => client.request<GetInvestmentQuery>(GetInvestmentDocument, variables, {...requestHeaders, ...wrappedRequestHeaders}), 'getInvestment', 'query');
+  query findAll {
+    findAll {
+      username
     }
+  }
+`;
+export const GetInvestmentDocument = gql`
+  query getInvestment {
+    getInvestment {
+      title
+      description
+      _id
+    }
+  }
+`;
+
+export type SdkFunctionWrapper = <T>(
+  action: (requestHeaders?: Record<string, string>) => Promise<T>,
+  operationName: string,
+  operationType?: string,
+) => Promise<T>;
+
+const defaultWrapper: SdkFunctionWrapper = (
+  action,
+  _operationName,
+  _operationType,
+) => action();
+
+export function getSdk(
+  client: GraphQLClient,
+  withWrapper: SdkFunctionWrapper = defaultWrapper,
+) {
+  return {
+    findAll(
+      variables?: FindAllQueryVariables,
+      requestHeaders?: Dom.RequestInit['headers'],
+    ): Promise<FindAllQuery> {
+      return withWrapper(
+        wrappedRequestHeaders =>
+          client.request<FindAllQuery>(FindAllDocument, variables, {
+            ...requestHeaders,
+            ...wrappedRequestHeaders,
+          }),
+        'findAll',
+        'query',
+      );
+    },
+    getInvestment(
+      variables?: GetInvestmentQueryVariables,
+      requestHeaders?: Dom.RequestInit['headers'],
+    ): Promise<GetInvestmentQuery> {
+      return withWrapper(
+        wrappedRequestHeaders =>
+          client.request<GetInvestmentQuery>(GetInvestmentDocument, variables, {
+            ...requestHeaders,
+            ...wrappedRequestHeaders,
+          }),
+        'getInvestment',
+        'query',
+      );
+    },
   };
 }
 export type Sdk = ReturnType<typeof getSdk>;
