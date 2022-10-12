562e3f0b02692d57218595927600fec542ebee3c
Lucas Zanek <lucaszanek94@gmail.com>
2022-10-12 11:45:15 -0300

    [CONFIG]: Pre commit hooks + linter + prettiers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { Hydrate, QueryClientProvider } from 'react-query';
 import { queryClient } from '../src/api';
 
 function MyApp({ Component, pageProps }: any) {
-  const t = '111';
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>
